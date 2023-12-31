7d79dfc6ba22dc93529ad375a958dcd765495fdc
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2023-12-31 12:47:45 +0900

    back to implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    api("org.jetbrains:annotations:24.1.0")
-    api("com.squareup.okhttp3:okhttp:4.12.0")
-    api("com.google.code.gson:gson:2.10.1")
-    api("org.java-websocket:Java-WebSocket:1.5.5")
+    implementation("org.jetbrains:annotations:24.1.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.java-websocket:Java-WebSocket:1.5.5")
 }
 
 tasks.test {
